b4283446b04e7a158385cbb26cbab2092aa0ba3e
RanungPark <ckehfqkr302@naver.com>
2023-06-26 17:29:38 +0900

    add devtools

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import './App.css';
 import MainProducts from './components/MainProducts';
-import { QueryClient, QueryClientProvider} from 'react-query'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
 
 const queryClient = new QueryClient()
 
@@ -9,6 +10,7 @@ export default function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <MainProducts />;
+      <ReactQueryDevtools initialIsOpen={true} />
     </QueryClientProvider>
   )
 }
